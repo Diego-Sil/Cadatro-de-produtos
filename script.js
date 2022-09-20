100f67a5d0afbaa5013cad41e7c3e951c7055a3a
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2022-09-19 22:34:30 -0300

    Adicionando  script.js e declarando variaveis

---
+++ script.js
@@ -0,0 +1,11 @@
+let codigo = document.getElementById('codigo');
+let fabricante = document.getElementById('fabricante');
+let referencia = document.getElementById('referencia');
+let descrição = document.getElementById('descrição');
+let quantidade_embalagem = document.getElementById('quantidade_embalagem');
+let preco = document.getElementById('preco');
+let ean = document.getElementById('ean');
+let btnSalvar = document.getElementById('salvar');
+let btnCorrigir = document.getElementById('corrigir');
+let btnExcluir = document.getElementById('excluir');
+
