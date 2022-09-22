b4ba21bb28d951cbb74e4eeb1a5dd3ed00004a43
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2022-09-22 03:07:23 -0300

    Exibindo os dados do form na tabela e se limpando

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let referencia = document.getElementById('referencia');
 let descrição = document.getElementById('descrição');
 let quantidade_embalagem = document.getElementById('quantidade_embalagem');
 let preco = document.getElementById('preco');
-let ean = document.getElementById('ean');
+let
+ean = document.getElementById('ean');
 let btnSalvar = document.getElementById('salvar');
 let btnCorrigir = document.getElementById('corrigir');
 let btnExcluir = document.getElementById('excluir');
@@ -14,18 +15,31 @@ let tabela = document.querySelector('tbody');
 
 form.addEventListener('submit',(event)=>{
 
+   
     event.preventDefault();
 
-    /* event.target.elements['codigo'].value;
-    event.target.elements['fabricante'].value;
-    event.target.elements['referencia'].value;
-    event.target.elements['descrição'].value;
-    event.target.elements['quantidade_embalagem'].value;
-    event.target.elements['preco'].value;
-    event.target.elements['ean'].value; */
+    const codigo = event.target.elements['codigo'];
+    const fabricante = event.target.elements['fabricante'];
+    const referencia = event.target.elements['referencia'];
+    const descrição = event.target.elements['descrição']
+    const quantidade_embalagem = event.target.elements['quantidade_embalagem'];
+    const preco = event.target.elements['preco'];
+    const ean = event.target.elements['ean'];
+
+    tabela.appendChild(criaTr(codigo.value, fabricante.value, referencia.value, descrição.value, quantidade_embalagem.value, preco.value, ean.value
+    ));
+    
+    codigo.value = ""
+    fabricante.value = ""
+    referencia.value = ""
+    descrição.value = ""
+    quantidade_embalagem.value = ""
+    preco.value = ""
+    ean.value = ""
 })
 
 btnCorrigir.addEventListener('click',function(event) {
+   
     event.preventDefault();
     tabela.appendChild(criaTr());
 })
@@ -36,16 +50,15 @@ function criaTd(dado){
     return td;
 }
 
-function criaTr(){
+function criaTr(cod, fabric, ref, desc, quantEmb, preco,
+    ean){
     const tr = document.createElement('tr');
-    tr.appendChild(criaTd('oiu'));
-    tr.appendChild(criaTd('oiu'));
-    tr.appendChild(criaTd('oiu'));
-    tr.appendChild(criaTd('oiu'));
-    tr.appendChild(criaTd('oiu'));
-
-
-
+    tr.appendChild(criaTd(cod));
+    tr.appendChild(criaTd(fabric));
+    tr.appendChild(criaTd(ref));
+    tr.appendChild(criaTd(desc));
+    tr.appendChild(criaTd(quantEmb));
+    tr.appendChild(criaTd(preco));
+    tr.appendChild(criaTd(ean));
     return tr;
 }
-
