9f49411634c576860971bf1c70647596d824e7a0
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2022-09-21 22:58:08 -0300

    Capturando os dados do form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const form = document.getElementById('novoItem');
+
 let codigo = document.getElementById('codigo');
 let fabricante = document.getElementById('fabricante');
 let referencia = document.getElementById('referencia');
@@ -9,3 +11,13 @@ let btnSalvar = document.getElementById('salvar');
 let btnCorrigir = document.getElementById('corrigir');
 let btnExcluir = document.getElementById('excluir');
 
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    console.log(event.target.elements['codigo'].value);
+    console.log(event.target.elements['fabricante'].value);
+    console.log(event.target.elements['referencia'].value);
+    console.log(event.target.elements['descrição'].value);
+    console.log(event.target.elements['quantidade_embalagem'].value);
+    console.log(event.target.elements['preco'].value);
+    console.log(event.target.elements['ean'].value);
+})
