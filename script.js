4036deffede470cebffa2a97c69ee0d438e08284
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2022-09-22 00:46:00 -0300

    Esta exibindo uma nova linha na tabela

--- script.js
+++ script.js
@@ -10,14 +10,42 @@ let ean = document.getElementById('ean');
 let btnSalvar = document.getElementById('salvar');
 let btnCorrigir = document.getElementById('corrigir');
 let btnExcluir = document.getElementById('excluir');
+let tabela = document.querySelector('tbody');
 
 form.addEventListener('submit',(event)=>{
+
     event.preventDefault();
-    console.log(event.target.elements['codigo'].value);
-    console.log(event.target.elements['fabricante'].value);
-    console.log(event.target.elements['referencia'].value);
-    console.log(event.target.elements['descrição'].value);
-    console.log(event.target.elements['quantidade_embalagem'].value);
-    console.log(event.target.elements['preco'].value);
-    console.log(event.target.elements['ean'].value);
+
+    /* event.target.elements['codigo'].value;
+    event.target.elements['fabricante'].value;
+    event.target.elements['referencia'].value;
+    event.target.elements['descrição'].value;
+    event.target.elements['quantidade_embalagem'].value;
+    event.target.elements['preco'].value;
+    event.target.elements['ean'].value; */
 })
+
+btnCorrigir.addEventListener('click',function(event) {
+    event.preventDefault();
+    tabela.appendChild(criaTr());
+})
+
+function criaTd(dado){
+    const td = document.createElement('td');
+    td.textContent = dado;
+    return td;
+}
+
+function criaTr(){
+    const tr = document.createElement('tr');
+    tr.appendChild(criaTd('oiu'));
+    tr.appendChild(criaTd('oiu'));
+    tr.appendChild(criaTd('oiu'));
+    tr.appendChild(criaTd('oiu'));
+    tr.appendChild(criaTd('oiu'));
+
+
+
+    return tr;
+}
+
